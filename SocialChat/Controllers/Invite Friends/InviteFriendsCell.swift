b91324d8bebddc932ed3370b1251e57dbf95cfad
m.saplel <kaebs19@gmail.com>
2023-11-15 03:27:17 +0300

    2.1

---
+++ SocialChat/Controllers/Invite Friends/InviteFriendsCell.swift
@@ -0,0 +1,30 @@
+//
+//  InviteFriendsCell.swift
+//  SocialChat
+//
+//  Created by mjeed on 06/11/2023.
+//
+
+import UIKit
+
+class InviteFriendsCell: UITableViewCell {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var imageUser: UIImageView!
+    @IBOutlet weak var nameUserLable: UILabel!
+    @IBOutlet weak var dateilsLable: UILabel!
+    @IBOutlet weak var ckickUser: UIImageView!
+    
+    // MARK: - Variables
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
---
+++ SocialChat/Controllers/Invite Friends/InviteFriendsVC.swift
@@ -0,0 +1,29 @@
+//
+//  InviteFriendsVC.swift
+//  SocialChat
+//
+//  Created by mjeed on 06/11/2023.
+//
+
+import UIKit
+
+class InviteFriendsVC: UIViewController {
+    
+    // MARK: - Outlets
+
+    
+    
+    
+    
+    // MARK: - Variables
+
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+}
